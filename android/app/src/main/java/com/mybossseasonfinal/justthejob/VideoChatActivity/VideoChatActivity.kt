44ef8672a9e0bc41c267002fcf1a75cfe80139c3
ytakaya <ytakaya@mikilab.doshisha.ac.jp>
2018-10-17 06:21:07 +0900

    とりあえず自身の顔をskywayで表示

--- android/app/src/main/java/com/mybossseasonfinal/justthejob/VideoChatActivity/VideoChatActivity.kt
+++ android/app/src/main/java/com/mybossseasonfinal/justthejob/VideoChatActivity/VideoChatActivity.kt
@@ -13,28 +13,41 @@ import android.view.Window
 import android.widget.Toast
 import com.mybossseasonfinal.justthejob.BuildConfig
 import com.mybossseasonfinal.justthejob.R
+import io.skyway.Peer.Browser.Canvas
 import io.skyway.Peer.Browser.MediaConstraints
+import io.skyway.Peer.Browser.MediaStream
 import io.skyway.Peer.Browser.Navigator
+import io.skyway.Peer.MediaConnection
 import io.skyway.Peer.Peer
+import io.skyway.Peer.PeerError
 import io.skyway.Peer.PeerOption
 import kotlinx.android.synthetic.main.activity_video_chat.*
 
 
 class VideoChatActivity : AppCompatActivity() {
     private lateinit var peer: Peer
+    private lateinit var localStream: MediaStream//自身のMediaStreamオブジェクト
+    private lateinit var remoteStream: MediaStream//相手のMediaStreamオブジェクト
+    private lateinit var mediaConnection: MediaConnection
+
+    private lateinit var ownId: String
+    private lateinit var handler: Handler
+    private var isConnected: Boolean = false
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         window.addFlags(Window.FEATURE_NO_TITLE)
         setContentView(R.layout.activity_video_chat)
 
-        val handler = Handler(Looper.getMainLooper())
+        handler = Handler(Looper.getMainLooper())
 
         val options = PeerOption()
         options.key = BuildConfig.SKYWAY_API_KEY
         options.domain = BuildConfig.SKYWAY_HOST
+        options.debug = Peer.DebugLevelEnum.ALL_LOGS
         peer = Peer(this, options)
-        Navigator.initialize(peer)
+
 
         showCurrentPeerId()
 
@@ -69,6 +82,25 @@ class VideoChatActivity : AppCompatActivity() {
             }
             runOnUiThread { textView_peerid.text = "ID: $currentPeerId" }
         }
+
+        //接続が切れた時に発火
+        peer.on(Peer.PeerEventEnum.CLOSE) {
+            Log.d("Skyway_TEST", "[On/Close]")
+        }
+
+        //シグナリングサーバとの接続が切れた時に発火
+        peer.on(Peer.PeerEventEnum.DISCONNECTED) {
+            Log.d("Skyway_TEST", "[On/Disconnected]")
+        }
+
+
+        //エラー時に発火
+        peer.on(Peer.PeerEventEnum.ERROR) { currentPeerId ->
+            val error = currentPeerId as PeerError
+            Log.d("Skyway_TEST", "[On/Error]$error")
+        }
+
+
     }
 
     private fun startLocalStream() {
@@ -76,5 +108,10 @@ class VideoChatActivity : AppCompatActivity() {
         mediaConstraints.maxWidth = 960
         mediaConstraints.maxHeight = 540
         mediaConstraints.cameraPosition = MediaConstraints.CameraPositionEnum.FRONT
+
+        Navigator.initialize(peer)
+        localStream = Navigator.getUserMedia(mediaConstraints)
+        val canvas = findViewById<Canvas>(R.id.svLocalView)
+        localStream.addVideoRenderer(canvas, 0)
     }
 }
