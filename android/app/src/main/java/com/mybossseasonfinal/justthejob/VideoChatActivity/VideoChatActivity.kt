6a6b5dcba95107434388435b3a1a2d2b4534998b
ytakaya <ytakaya@mikilab.doshisha.ac.jp>
2018-10-16 12:07:16 +0900

    activity追加

---
+++ android/app/src/main/java/com/mybossseasonfinal/justthejob/VideoChatActivity/VideoChatActivity.kt
@@ -0,0 +1,13 @@
+package com.mybossseasonfinal.justthejob.VideoChatActivity
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import com.mybossseasonfinal.justthejob.R
+
+class VideoChatActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_video_chat)
+    }
+}
