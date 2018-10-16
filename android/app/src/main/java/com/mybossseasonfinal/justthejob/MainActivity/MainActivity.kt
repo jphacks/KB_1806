dc410e9f7bad0320f2f8a53f14d1521b5a507010
ytakaya <ytakaya@mikilab.doshisha.ac.jp>
2018-10-16 09:52:41 +0900

    ToolbarにNavigationDrawerとOptionの設置

--- android/app/src/main/java/com/mybossseasonfinal/justthejob/MainActivity/MainActivity.kt
+++ android/app/src/main/java/com/mybossseasonfinal/justthejob/MainActivity/MainActivity.kt
@@ -1,14 +1,22 @@
 package com.mybossseasonfinal.justthejob.MainActivity
 
 import android.os.Bundle
+import android.support.design.widget.NavigationView
+import android.support.v4.view.GravityCompat
+import android.support.v4.widget.DrawerLayout
+import android.support.v7.app.ActionBarDrawerToggle
 import android.support.v7.app.AppCompatActivity
+import android.support.v7.widget.Toolbar
+import android.util.Log
+import android.view.Menu
+import android.view.MenuItem
 import com.mybossseasonfinal.justthejob.DI.Component.DaggerActivityComponent
 import com.mybossseasonfinal.justthejob.DI.Module.ActivityModule
 import com.mybossseasonfinal.justthejob.JustTheJobApp
 import com.mybossseasonfinal.justthejob.R
 import javax.inject.Inject
 
-class MainActivity : AppCompatActivity(), MainContract.View {
+class MainActivity : AppCompatActivity(), MainContract.View, NavigationView.OnNavigationItemSelectedListener {
 
     @Inject
     lateinit var mainPresenter: MainPresenter
@@ -24,5 +32,44 @@ class MainActivity : AppCompatActivity(), MainContract.View {
 
         mainPresenter.apiPost()
 
+        //Toolbar
+        val toolbar = findViewById<Toolbar>(R.id.toolbar)
+        setSupportActionBar(toolbar)
+
+        //DrawerToggle
+        val drawer = findViewById<DrawerLayout>(R.id.drawerLayout)
+        val toggle = ActionBarDrawerToggle(this, drawer, toolbar, R.string.drawer_open, R.string.drawer_close)
+        drawer.addDrawerListener(toggle)
+        toggle.syncState()
+
+        //NavigationView Listener
+        val navigationView = findViewById<NavigationView>(R.id.navigationView)
+        navigationView.setNavigationItemSelectedListener(this)
+
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.options, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem?): Boolean {
+        when (item?.itemId) {
+            R.id.action_settings -> Log.d("MainActivity", "Settings Selected!")
+        }
+        return true
+    }
+
+
+    override fun onNavigationItemSelected(item: MenuItem): Boolean {
+        when (item.itemId) {
+            R.id.menu_item1 -> Log.d("MainActivity", "1 Selected")
+            R.id.menu_item2 -> Log.d("MainActivity", "2 Selected")
+            R.id.menu_item3 -> Log.d("MainActivity", "3 Selected")
+            R.id.menu_item4 -> Log.d("MainActivity", "4 Selected")
+        }
+        val drawer = findViewById<DrawerLayout>(R.id.drawerLayout)
+        drawer.closeDrawer(GravityCompat.START)
+        return true
     }
 }
