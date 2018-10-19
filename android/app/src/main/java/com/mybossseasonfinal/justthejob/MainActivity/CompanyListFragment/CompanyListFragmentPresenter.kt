42868cb373e6f75d4281ebe8bccf2a1232900221
yyh-gl <yhonda.95.gl@gmail.com>
2018-10-19 14:47:14 +0900

    テストデータ修正

--- android/app/src/main/java/com/mybossseasonfinal/justthejob/MainActivity/CompanyListFragment/CompanyListFragmentPresenter.kt
+++ android/app/src/main/java/com/mybossseasonfinal/justthejob/MainActivity/CompanyListFragment/CompanyListFragmentPresenter.kt
@@ -10,9 +10,9 @@ class CompanyListFragmentPresenter @Inject constructor(
 
     fun getMatchingCompanyList(): MutableList<Company> {
         return mutableListOf(
-                Company(_id = "1", id = 1, name = "NTT docomo", founder = "吉澤社長", founding = "1993", address = "溜池山王"),
-                Company(_id = "2", id = 2, name = "野村総合研究所", founder = "？？？", founding = "????", address = "横浜みなとみらい"),
-                Company(_id = "3", id = 3, name = "NTTデータ", founder = "？？？", founding = "????", address = "豊洲")
+                Company(_id = "1", id = 1, name = "NTT docomo", founder = "吉澤社長", founding = "1993", address = "溜池山王", img_path = ""),
+                Company(_id = "2", id = 2, name = "野村総合研究所", founder = "？？？", founding = "????", address = "横浜みなとみらい", img_path = ""),
+                Company(_id = "3", id = 3, name = "NTTデータ", founder = "？？？", founding = "????", address = "豊洲", img_path = "")
         )
     }
 }
