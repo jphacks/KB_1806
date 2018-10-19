4f3ae624c8fd2f28802982bd9684103997f60497
yyh-gl <yhonda.95.gl@gmail.com>
2018-10-19 18:32:32 +0900

    My企業追加API作成

--- backend/routes/users.js
+++ backend/routes/users.js
@@ -50,4 +50,24 @@ router.post('/login', function(req, res) {
     res.json(req.body);
 });
 
+//社員個人を送信
+router.post('/:user_id/company/:company_id', function(req, res, next) {
+    postUserCompany(req.params.user_id, req.params.company_id, "company", "users_company", res);
+});
+
+function postUserCompany(user_id, company_id, dbName, collection, res){
+    // MongoDB へ 接続
+    MongoClient.connect(url_db, (error, client) => {
+        const db = client.db(dbName);
+
+        // コレクションの取得
+        collection = db.collection(collection);
+        collection.insert({user_id:Number(user_id), company_id:Number(company_id)}).toArray((error, documents)=>{
+            if(documents[0] != null) res.json(documents[0]);
+            else res.send("error:id not found");
+        });
+    });
+};
+
+
 module.exports = router;
