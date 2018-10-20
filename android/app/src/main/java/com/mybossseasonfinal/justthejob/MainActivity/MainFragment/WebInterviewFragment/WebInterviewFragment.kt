ea7832717c449b5856c3946c750a4a8afa77e4e8
ytakaya <ytakaya@mikilab.doshisha.ac.jp>
2018-10-20 13:06:21 +0900

    Web面接会場入り口の機能のみ実装

--- android/app/src/main/java/com/mybossseasonfinal/justthejob/MainActivity/MainFragment/WebInterviewFragment/WebInterviewFragment.kt
+++ android/app/src/main/java/com/mybossseasonfinal/justthejob/MainActivity/MainFragment/WebInterviewFragment/WebInterviewFragment.kt
@@ -1,10 +1,14 @@
 package com.mybossseasonfinal.justthejob.MainActivity.MainFragment.WebInterviewFragment
 
+import android.content.Intent
 import android.os.Bundle
 import android.support.v4.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
 import com.mybossseasonfinal.justthejob.R
 
 class WebInterviewFragment : Fragment() {
@@ -18,6 +22,21 @@ class WebInterviewFragment : Fragment() {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_web_interview, container, false)
     }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        val textViewRoomName = view.findViewById<TextView>(R.id.textView_room_name)
+        val editTextInterviewId = view.findViewById<EditText>(R.id.editText_interview_id)
+        val buttonEnterInterview = view.findViewById<Button>(R.id.button_enter_interview)
+
+        buttonEnterInterview.setOnClickListener {
+            val intent = Intent(activity, WebInterviewActivity::class.java)
+            startActivity(intent)
+        }
+
+    }
+
 
     companion object {
         fun createInstance(): WebInterviewFragment {
